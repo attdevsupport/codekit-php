31c8b9f606ebe046c079b276e7defd5bf2110bf3
attdevsupport <developer.support@att.com>
2015-08-28 14:35:14 -0700

    Codekit PHP updates - 08/28/2015

--- integration_tests/cfgs/aab_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$api_key = "1234"; 
-$secret_key = "1234";
-$FQDN = 'localhost:8888';
-# $proxy_host = 'proxy.host';
-# $proxy_port = 8080;
-# $accept_all_certs = true;
-?>
--- integration_tests/cfgs/dc_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$FQDN = 'https://api.att.com';
-$accessToken="";
-
-# $proxyHost = 'proxy.host';
-# $proxyPort = 8080;
-# $allowAllCerts = true;
-?>
