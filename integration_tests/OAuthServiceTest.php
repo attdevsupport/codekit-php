d3df27982e766a0087e461973ac09e00ec94b8a5
attdevsupport <developer.support@att.com>
2016-06-08 15:14:27 -0700

    Deprecate ADS

--- integration_tests/OAuthServiceTest.php
+++ integration_tests/OAuthServiceTest.php
@@ -7,7 +7,7 @@ class OAuthTokenServiceTest extends PHPUnit_Framework_TestCase
 {
     public function testRequest() 
     {
-        require __DIR__ . '/cfgs/ads_config.php';
+        require __DIR__ . '/cfgs/sms_config.php';
 
         if (isset($proxyHost) && isset($proxyPort))
             RestfulEnvironment::setProxy($proxyHost, $proxyPort);
@@ -15,7 +15,7 @@ class OAuthTokenServiceTest extends PHPUnit_Framework_TestCase
             RestfulEnvironment::setAcceptAllCerts($allowAllCerts);
 
         $tokenSrvc = new OAuthTokenService($FQDN, $api_key, $secret_key);
-        $token = $tokenSrvc->getToken('ADS');
+        $token = $tokenSrvc->getToken('SMS');
 
         $this->assertTrue($token->getAccessToken() != '');
 
